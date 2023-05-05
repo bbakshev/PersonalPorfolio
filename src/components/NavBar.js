31584d11b2b5a9eed931a5e981dcc13732731785
Brishna Bakshev <b.bakshev@gmail.com>
2023-05-05 11:36:50 -0700

    added assets and completed the navbar code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,29 @@
-import {NavBar, Container} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { NavBar, Container } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      }
+      else {
+        setScrolled(false);
+      }
+    }
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (link) => {
+    setActiveLink(link);
+  }
+
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
