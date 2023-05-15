189b68705bc6de216ea449ff3504329ef29153dc
Brishna Bakshev <b.bakshev@gmail.com>
2023-05-15 15:49:28 -0700

    changed the speed of typing for the h1

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Hi, I'm Brishna!","Web Developer", "Frontend Developer" ];
+  const toRotate = ["Hi, I'm Brishna!", "Web Developer", "Frontend Developer"];
   const period = 1000;
 
   useEffect(() => {
@@ -18,33 +18,44 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
+      if (isDeleting) {
+        // Adjust the speed of the deletion phase here
+        setDelta(100);
+      } else {
+        // Adjust the speed of the typing phase here
+        setDelta(50);
+      }
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -52,25 +63,36 @@ export const Banner = () => {
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                {/* <span className="tagline">Welcome to my Portfolio</span> */}
-                <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Hi, Im Brishna!","Web Developer", "Frontend Developer" ]'><span className="wrap">{text}</span></span></h1>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  {/* <span className="tagline">Welcome to my Portfolio</span> */}
+                  <h1>
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="300"
+                      data-rotate='["Hi, Im Brishna!", "Web Developer", "Frontend Developer"]'>
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
                   <p>I am a Seattle-based front-end React Developer actively seeking a new opportunity.</p>
-                  <a href="https://drive.google.com/file/d/1rsm3XnPS8VCM2V2ObIHKH1znLsMh7M40/view?usp=share_link" target="_blank" rel="noopener noreferrer"><button className="cv-btn">Download CV</button></a>
-              </div>}
+                  <a href="https://drive.google.com/file/d/1rsm3XnPS8VCM2V2ObIHKH1znLsMh7M40/view?usp=share_link" target="_blank" rel="noopener noreferrer">
+                    <button className="cv-btn">Download CV</button>
+                  </a>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
+          <TrackVisibility>
+              {({ isVisible }) => (
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
                   <img src={headerImg} alt="Header Img"/>
-                </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
