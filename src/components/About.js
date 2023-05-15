9cf8073d85046c3f8481e51f30191f2766a46717
Brishna Bakshev <b.bakshev@gmail.com>
2023-05-15 15:52:49 -0700

    updates to my about me

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export const About = () => {
             <div className="about-bx">
             <h2>About Me</h2>
             <p className="para">
-            As a dedicated full stack developer, my enthusiasm for learning and creating is limitless. Recently graduated from a coding bootcamp, I have gained practical experience in React, JavaScript, HTML, CSS, and C#. I possess a natural talent for creatively solving intricate problems and thrive in collaborative team settings. The joy of continuous growth and expanding my skill set drives my excitement to flourish as a developer.</p>
+            As a dedicated full stack developer, my enthusiasm for learning and creating is limitless. Recently graduated from a coding bootcamp, I have gained practical experience in React, JavaScript, HTML, CSS, and C#. I possess a natural talent for creatively solving intricate problems and thrive in collaborative team settings. The joy of continuous growth and expanding my skill set drives my excitement to flourish as a developer. In my free time I love the outdoors, being active, playing video games, film photography, as well as connecting with friends and family. </p>
             </div>
         </div>
       </div>
